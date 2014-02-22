a84a00913c7c2bd54fa1c80f013f9b016f8b23ff
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-02-22 12:47:08 -0600

    Remove scm from base image

--- base/setup.sh
+++ base/setup.sh
@@ -5,12 +5,3 @@ set -e
 
 # Install dependencies
 apt-get install -y sudo python-software-properties curl wget
-
-# Add SCMs repositories
-add-apt-repository -y ppa:git-core/ppa
-add-apt-repository -y ppa:mercurial-ppa/releases
-add-apt-repository -y ppa:svn/ppa
-apt-get update -y
-
-# Install SCMs
-apt-get install -y git mercurial subversion
